ad46398d8d22cf905bc8abdc0b76d6e0e76d3a46
Duc-267 <mduc2672002@gmail.com>
2022-11-25 22:20:11 +0700

    Load data file

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SlangDictionary slangDictionary = new SlangDictionary();
+        System.out.println(slangDictionary.slangDictionary.size());
+    }
+}
---
+++ src/SlangDictionary.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class SlangDictionary {
+    HashMap<String, List<String>> slangDictionary;
+    public SlangDictionary() {
+        slangDictionary = new HashMap<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("slang.txt"));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if(line.contains("`")) {
+                    String[] words = line.split("`");
+                    String slang = words[0];
+                    String definitions = words[1];
+                    String[] definition = definitions.split("\\|");
+                    List<String> definitionList = new ArrayList<>();
+                    for (String def : definition) {
+                        def.trim();
+                        definitionList.add(def);
+                    }
+                    slangDictionary.put(slang, definitionList);
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void printAll() {
+        for (String slang : slangDictionary.keySet()) {
+            List<String> meanings = slangDictionary.get(slang);
+            System.out.print(slang + ":");
+            for (String meaning : meanings) {
+                System.out.print(meaning);
+            }
+            System.out.println();
+        }
+    }
+    
+}
